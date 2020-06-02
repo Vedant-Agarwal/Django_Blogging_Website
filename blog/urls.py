4beb7b227e3d31d48bcf27a4f74da78ea6a2761c
Vedant Agarwal <agarwal.ve@husky.neu.edu>
2020-06-02 01:45:22 -0400

    Edits
    Video11

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,20 @@
 from django.conf.urls import url
+from .views import PostListView, PostDetailView
 from django.contrib import admin
-
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 
 urlpatterns = [
-    url('', views.home, name='blog-home'),
+    url('', PostListView.as_view(), name='blog-home'),
+    url('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    url('post/new/', PostCreateView.as_view(), name='post-create'),
+    url('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    url('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     url('about/', views.about, name='blog-about'),
 ]
